024c3348dd3df39b5c7bc31c0461dd1ff38ce1ea
nadlabak <pavel@doshaska.net>
2011-02-03 19:10:49 +0100

    oops, fix storage notification
    Change-Id: I70680bd3ad5ff5f7d294331d3b134696006a6991

--- packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java
+++ packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java
@@ -34,7 +34,7 @@ public class StorageNotification extends StorageEventListener {
     private static final String TAG = "StorageNotification";
 
     private static final boolean POP_UMS_ACTIVITY_ON_CONNECT = false;
-    private static final boolean UMS_NOTIFICATION_VISIBLE = false
+    private static final boolean UMS_NOTIFICATION_VISIBLE = false;
 
     /**
      * Binder context for this service
