ab56a0d431c3ce794177b04c339e752af26d5fe8
nadlabak <pavel@doshaska.net>
2011-04-05 20:06:30 +0200

    allow no action on usb/power plug - fix parenthesis
    Change-Id: Ib8c1db909b42a1fb64d623d72707467cdf2695de

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -417,7 +417,7 @@ class PowerManagerService extends IPowerManager.Stub
                     // turn on.  Some devices want this because they don't have a
                     // charging LED.
                     synchronized (mLocks) {
-                        if ("0".equals(SystemProperties.get("persist.sys.no_action_on_plug", "0") &&
+                        if ("0".equals(SystemProperties.get("persist.sys.no_action_on_plug", "0")) &&
                                 (!wasPowered || (mPowerState & SCREEN_ON_BIT) != 0 ||
                                 mUnplugTurnsOnScreen)) {
                             forceUserActivityLocked();
