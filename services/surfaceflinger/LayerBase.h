057f39e870f2be8c2339118a5c83d8dd923b687d
nadlabak <pavel@doshaska.net>
2011-04-24 00:50:55 +0200

    Revert "Revert "partially fix [3306150] HTML5 video with H/W acceleration blackout""
    This reverts commit fceeb1f609223d5b16fc7649dcefe1fdcb9997a2.
    
    - it doesn't solve the black screen issue on Milestone

--- services/surfaceflinger/LayerBase.h
+++ services/surfaceflinger/LayerBase.h
@@ -213,6 +213,7 @@ public:
     
     /** always call base class first */
     virtual void dump(String8& result, char* scratch, size_t size) const;
+    virtual void shortDump(String8& result, char* scratch, size_t size) const;
 
 
     enum { // flags for doTransaction()
@@ -334,6 +335,7 @@ public:
 
 protected:
     virtual void dump(String8& result, char* scratch, size_t size) const;
+    virtual void shortDump(String8& result, char* scratch, size_t size) const;
 
 private:
     mutable Mutex mLock;
